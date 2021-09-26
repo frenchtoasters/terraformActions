76cd2f855c43163be6f4d1b9ace7423ae55f6e73
toast <toast@big-toaster>
2021-09-26 18:09:13 -0400

    running tf fmt

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "LINODE_TOKEN" {
-	type = string
+  type = string
 }
 
 variable "OBJ_REGION" {
-	type = string
+  type = string
 }
 
 variable "OBJ_ACCESS_KEY" {
-	type = string
+  type = string
 }
 
 variable "OBJ_SECRET_KEY" {
-	type = string
+  type = string
 }
 
 variable "OBJ_BUCKET_URL" {
-	type = string
+  type = string
 }
