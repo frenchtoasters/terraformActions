23f52fbeea5378b5210657463ee689a65cb35b37
toast <toast@big-toaster>
2021-09-26 12:45:19 -0400

    Starting project

---
+++ cluster.tf
@@ -0,0 +1,19 @@
+provider "linode" {
+  token = var.LINODE_TOKEN
+}
+
+resource "linode_lke_cluster" "my-cluster" {
+  k8s_version = "1.21"
+  label = "my-terraform-cluster"
+  region = "us-central"
+  pool {
+    count = 1
+    type = "g6-standard-1"
+  }
+}
+
+resource "local_file" "kube_config_yaml" {
+  filename = "./my-cluster-kubeconfig.yaml"
+  content = base64decode(linode_lke_cluster.my-cluster.kubeconfig)
+}
+
